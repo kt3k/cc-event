fd0f9584b44bf59a0855d48878d2544950ed2323
Yoshiya Hinosawa <stibium121@gmail.com>
2015-10-03 20:14:38 +0900

    Add spec

---
+++ cc-event-spec.js
@@ -0,0 +1,45 @@
+
+
+
+
+
+describe('Function.prototype.event', function () {
+    'use strict';
+
+    it('adds __events__ property to the function', function () {
+    });
+
+    it('pushes the given event and selector to the __events__ array', function () {
+    });
+
+});
+
+describe('$.cc.initEvents', function () {
+    'use strict';
+
+    it('sets event hanlders to this.elem according to the __events__ of the methods', function () {
+    });
+
+    it('ignores the items of __events__ if the given handler is not a function', function () {
+    });
+
+    it('does nothing if __events__ does not exist', function () {
+    });
+
+});
+
+describe('$.cc.Coelement', function () {
+    'use strict';
+
+    it('inits events at construction', function () {
+    });
+
+});
+
+describe('$.cc.Actor', function () {
+    'use strict';
+
+    it('inits events at construction', function () {
+    });
+
+});
